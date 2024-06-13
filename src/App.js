7f5e2510ef7bf9ef0abf7c0df4b9d5df8d17be25
arbajkhan23 <126312082+arbajkhan23@users.noreply.github.com>
2024-06-13 21:03:41 +0530

    new file

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import logo from './logo.svg';
+import './App.scss';
+
+function App() {
+  return (
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+    
+    <div className='flex '>
+<div className='box-1 mt-3 m-3'>
+</div>
+<div className='box-1 mt-3  mx-3'>
+</div>
+<div className='box-1 mt-3  mx-3'>
+</div>
+    </div>
+  );
+  
+}
+
+export default App;
