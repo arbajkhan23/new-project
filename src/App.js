841d6a29e56bec7908f3fe94f0d72f786a0f1e70
arbajkhan23 <126312082+arbajkhan23@users.noreply.github.com>
2024-06-15 12:14:42 +0530

    new change

--- src/App.js
+++ src/App.js
@@ -1,41 +1,21 @@
-
+import { BrowserRouter , Route, Routes } from 'react-router-dom';
 import './App.scss';
-
+import Header from './components/Header.js';
+import Home from './pages/home.js';
+import About from './pages/About.js';
 function App() {
   return (
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
-    
-    <div className='flex justify-between flex-wrap mx-4 gap-y-3'>
-<div className='sm:w-4/12 w-full px-4'>
-<div className='box-1'></div>
-</div>
-<div className='sm:w-4/12 w-full px-4'>
-<div className='box-1'>
-
-</div>
-</div>
-<div className=' sm:w-4/12 w-full px-4'>
-<div className='box-1'>
-</div>
-</div>
-    </div>
+  <>
+   <Header/>
+   <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home/>}></Route>
+      <Route path='/About' element={<About/>}>
+      </Route>
+    </Routes>
+    </BrowserRouter>
+   
+   </>
   );
-  
 }
-
 export default App;
---
+++ src/pages/About.js
@@ -0,0 +1,8 @@
+function about(){
+    return(
+        <>
+    <h1>about us</h1>
+        </>
+    )
+}
+export default about;
