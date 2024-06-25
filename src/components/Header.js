5058a6d79332fba4350a0a60153f1dbd01ae2509
arbajkhan23 <126312082+arbajkhan23@users.noreply.github.com>
2024-06-25 20:09:33 +0530

    dorp down

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,25 @@
+import React from "react";
+import { useState } from "react";
+
 function Header(){
+    const [showMenu, setShowMenu] = useState(false);
     return(
         <header className="bg-black">
             <div className="container mx-auto">
                 <nav className="flex justify-between items-center">
                     <a href="#" className="text-white">logo</a>
                     <ul>
-                        <li className="inline-block"><a href="/" className="text-white block p-3">Home</a></li>
-                        <li className="inline-block"><a href="/About" className="text-white block p-3">About</a></li>
-                        <li className="inline-block"><a href="#" className="text-white block p-3">Services</a></li>
+                        <li className="inline-block relative">
+                            <a href="/" className="text-white block p-3" >Home</a>
+                           
+                            </li>
+                        <li className="inline-block"><a href="#" className="text-white block p-3" onClick={() => setShowMenu(!showMenu)}>About</a>
+                        {showMenu && ( <ul className="absolute bg-white">
+                                <li><a href="#" className="text-balck  py-2">Home 1</a></li>
+                                <li><a href="#" className="text-balck  py-2">Home 2</a></li>
+                                <li><a href="#" className="text-balck  py-2">Home 3</a></li>
+                            </ul>)}</li>
+                        <li className="inline-block"><a href="/About" className="text-white block p-3">Services</a></li>
                         <li className="inline-block"><a href="#" className="text-white block p-3">Contact</a></li>
                         <li className="inline-block"><a href="#" className="text-white block p-3">Blog</a></li>
                     </ul>
